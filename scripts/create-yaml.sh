ba883c771517a0bf7b2b48cff77572a34d239f9a
Sean Sundberg <seansund@us.ibm.com>
2022-01-10 14:42:17 -0600

    Adds namespace to create secret
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -4,12 +4,14 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
 NAME="$1"
-DEST_DIR="$2"
+NAMESPACE="$2"
+DEST_DIR="$3"
 
 mkdir -p "${DEST_DIR}"
 
 kubectl create secret docker-registry \
   "${NAME}" \
+  -n "${NAMESPACE}" \
   --docker-server="${SERVER}" \
   --docker-username="${USERNAME}" \
   --docker-password="${PASSWORD}" \
